d057937f4757f55a64ee5e9003ae6ac8eef70bb4
turuslan <turuslan.devbox@gmail.com>
2025-08-21 19:01:52 +0500

    config

--- example/00-vcpkg-install/vcpkg-overlay/leanp2p/portfile.cmake
+++ example/00-vcpkg-install/vcpkg-overlay/leanp2p/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/leanp2p
-  REF fabc481353506283629ff7be3eb658e0c53b9eee
-  SHA512 4d11ca72fe258c5bff259d81061d1bc1bd72b96ba35f1e800a2412a62c465e49199ecfab0e023bc8f6b96bbd0ff31c29c3ececff3d5621008173dcf64ef3daef
+  REF ad6b90667d1e0c9f46fb521bd50412ebe1208ad3
+  SHA512 2d6781cabbc3aa688a07e6ad333e1d3fd11c32e02072a91d1f2188d6826238ad8bfed18b9107ae843d9f78bebc2d8e907f2b5d5f8f7db8d4a1726dc8e4693ebf
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
