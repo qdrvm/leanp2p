ed5b85dcaaff143d207ae5afe7de3e844a86d338
turuslan <turuslan.devbox@gmail.com>
2025-09-01 08:43:05 +0500

    fix channel

--- include/libp2p/coro/channel.hpp
+++ include/libp2p/coro/channel.hpp
@@ -6,7 +6,6 @@
 
 #pragma once
 
-#include <boost/asio/detached.hpp>
 #include <boost/asio/experimental/channel.hpp>
 #include <libp2p/coro/asio.hpp>
 #include <libp2p/coro/coro.hpp>
@@ -23,7 +22,11 @@ namespace libp2p {
     void send(outcome::result<T> result) {
       channel_.async_send(boost::system::error_code{},
                           std::move(result),
-                          boost::asio::detached);
+                          [](boost::system::error_code ec) {
+                            if (ec) {
+                              throw std::system_error{ec};
+                            }
+                          });
     }
 
     boost::asio::experimental::channel<void(boost::system::error_code,
